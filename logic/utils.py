697a4d7f4fc24358c5f09fb68bf87d63dd3f174d
mukunda1002 <mukundamangroliya1002@gmail.com>
2025-01-01 18:35:18 +0530

    testing black

--- logic/utils.py
+++ logic/utils.py
@@ -6,3 +6,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def sum(a, b):
+    # sum function
+    return a + b
