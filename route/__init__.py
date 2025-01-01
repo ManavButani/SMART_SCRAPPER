0b455b5353b84a0d8f6f504fcf4bd63d05ea5460
mukunda1002 <mukundamangroliya1002@gmail.com>
2025-01-01 13:01:58 +0530

    ignore

--- route/__init__.py
+++
@@ -1,9 +0,0 @@
-from core.app import app
-from . import auth
-from . import user
-
-# Include the authentication router with the prefix "/api/auth"
-app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
-
-# Include the user management router with the prefix "/api"
-app.include_router(user.router, prefix="/api", tags=["users"])
